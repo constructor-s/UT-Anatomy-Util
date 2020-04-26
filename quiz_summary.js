3f9fb83ec672d8696fa452bd397c12eb01eb6234
Bill <shirunjie@126.com>
2020-04-25 23:09:14 -0400

    Add images, hint, etc.

--- quiz_summary.js
+++ quiz_summary.js
@@ -14,12 +14,11 @@ async function processScene(scene) {
 async function processQuizSlide(slide, slideData) {
     const title = slide["title"];
     const texts = [slide["interactions"][0]["lmstext"]];
-    const imageSources = ["TODO.png"];
     let choices;
-    if (slide["interactions"][0]["choices"] != undefined) 
-        choices = slide["interactions"][0]["choices"].map(choice => choice["lmstext"]);
-    else
-        choices = ["Error: Bad data format"]
+    // if (slide["interactions"][0]["choices"] != undefined) 
+    choices = slide["interactions"][0]["choices"].map(choice => choice["lmstext"]);
+    // else
+        // choices = ["Error: Bad data format"]
     
     const choiceIndex = new Map();
     slide["interactions"][0]["choices"].forEach(choice => choiceIndex.set(choice["id"], choice["lmstext"]));
@@ -32,12 +31,53 @@ async function processQuizSlide(slide, slideData) {
         }
     })
 
+    // Parse slideData JSON
+    // Find the base layer
+    let images = [];
+    let section = undefined;
+    const baseLayers = slideData["slideLayers"].filter(layer => layer["isBaseLayer"]);
+    if (baseLayers.length != 1) {
+        console.error("Expects only one base layer, found:", baseLayers.length);
+    } else {
+        const baseLayer = baseLayers.pop();
+        const imageObjects = baseLayer["objects"].filter(obj => obj["kind"] == "image");
+        images = imageObjects.map(obj => path.join("mobile", obj["data"]["html5data"]["url"]));
+        
+        section = baseLayer["objects"]
+            .filter(obj => "textLib" in obj)
+            .map(obj => obj["data"]["vectorData"]["altText"])
+            .filter(text => text.toLowerCase().includes("quiz"))
+            .pop();
+        // console.log(JSON.stringify(textObjects));
+    }
+
+    let comments = [];
+    const commentLayers = slideData["slideLayers"].filter(layer => !layer["isBaseLayer"]);
+    if (commentLayers.length >= 2) {
+        let commentLayer = commentLayers[1]; // usually 3, but we have removed the base layer just in case
+        let textObjects = commentLayer["objects"].filter(obj => "textLib" in obj);
+        comments = textObjects.map(obj => obj["data"]["vectorData"]["altText"]);
+        if (!comments.includes("Correct")) {
+            commentLayer = commentLayers[0]; // usually 3, but we have removed the base layer just in case
+            textObjects = commentLayer["objects"].filter(obj => "textLib" in obj);
+            comments = textObjects.map(obj => obj["data"]["vectorData"]["altText"]);
+        }
+        if (!comments.includes("Correct")) {
+            comments = [];
+        }
+        console.log(comments);
+    } else {
+        console.error("Cannot find the comment layer, expected at least 2 non base layers, found:", commentLayers.length);
+    }
+
     return {
+        "section": section,
         "title": title,
         "texts": texts,
-        "imageSources": imageSources,
+        "images": images,
         "choices": choices,
-        "answers": answers
+        "answers": answers,
+        "comments": comments
     };
 }
 
@@ -72,15 +112,17 @@ async function generateQuizSummary(moduleFolderRoot, jsonFolderRoot) {
     elem.appendChild(document.createTextNode(document.title));
     document.body.appendChild(elem);
     for (const qGroup of quizzesProcessed) {
-        {
-            const elem = document.createElement("h2");
-            elem.appendChild(document.createTextNode("Quiz"));
-            document.body.appendChild(elem);
-        }
+        const qGroupTitle = document.createElement("h2");
+        const qGroupTextNode = document.createTextNode("Quiz");
+        qGroupTitle.appendChild(qGroupTextNode);
+        document.body.appendChild(qGroupTitle);
+        
         const elem = document.createElement("section");
-        elem.className = "questiongroup";
+        elem.className = "question-group";
         document.body.appendChild(elem);
         for (const q of qGroup) {
+            qGroupTitle.textContent = q["section"];
+
             const qElem = document.createElement("div");
             qElem.className = "question";
             elem.appendChild(qElem);
@@ -94,9 +136,22 @@ async function generateQuizSummary(moduleFolderRoot, jsonFolderRoot) {
             texts.className = "texts";
             texts.appendChild(document.createTextNode(q["texts"].join("\n")));
             qElem.appendChild(texts);
+
+            const images = document.createElement("div");
+            images.className = "images"
+            qElem.appendChild(images);
+            q["images"].forEach(src => {
+                imgA = document.createElement("a");
+                imgA.setAttribute("href", src);
+                imgA.setAttribute("target", target="_blank");
+                images.appendChild(imgA);
+                img = document.createElement("img");
+                img.setAttribute("src", src);
+                imgA.appendChild(img);
+            })
             
             const choiceTitle = document.createElement("p");
-            choiceTitle.className = "choiceTitle";
+            choiceTitle.className = "choice-title";
             choiceTitle.appendChild(document.createTextNode("Choices:"));
             qElem.appendChild(choiceTitle);
 
@@ -110,7 +165,7 @@ async function generateQuizSummary(moduleFolderRoot, jsonFolderRoot) {
             qElem.appendChild(choices);
             
             const answerTitle = document.createElement("p");
-            answerTitle.className = "answerTitle";
+            answerTitle.className = "answer-title";
             answerTitle.appendChild(document.createTextNode("Answers:"));
             qElem.appendChild(answerTitle);
 
@@ -122,11 +177,27 @@ async function generateQuizSummary(moduleFolderRoot, jsonFolderRoot) {
                 answers.appendChild(item);
             });
             qElem.appendChild(answers);
+
+            const commentTitle = document.createElement("p");
+            commentTitle.className = "comment-title";
+            commentTitle.appendChild(document.createTextNode("Comments:"));
+            qElem.appendChild(commentTitle);
+
+            const comments = document.createElement("ul");
+            comments.className = "comments";
+            q["comments"].forEach(comment => {
+                const item = document.createElement("li");
+                item.appendChild(document.createTextNode(comment))  
+                comments.appendChild(item);
+            });
+            qElem.appendChild(comments);
         }
     }
 
     // console.log(dom.serialize());
-    return fs.writeFile(jsonFolderRoot.split("/").pop() + "_quiz.html", beautify(dom.serialize()));
+    console.log("Writing to:", path.join(moduleFolderRoot, jsonFolderRoot.split("/").pop() + "_quiz.html"));
+    return fs.writeFile(path.join(moduleFolderRoot, jsonFolderRoot.split("/").pop() + "_quiz.html"), 
+                        beautify(dom.serialize()));
 }
 
 async function main() {
